716181b9f7f42d72c7e0f3d53cc63407085d5682
MichelleMHey <michellemhey@gmail.com>
2013-05-06 10:38:07 -0500

    Updated and finished. Added security and tags.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,18 @@
 class CommentsController < ApplicationController
-  def creat
-   @post = Post.find(params[:comment])
-   redirect_to post_path(@post)
+  
+  http_basic_authenticate_with :name => "dhh", :password => "secret", :only => :destroy
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(params[:comment])
+    redirect_to post_path(@post)
   end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
 end
